95a915b96004da403c8ec6c60194875624d89f39
Robert McCabe <rmccabe1023@gmail.com>
2024-03-26 22:16:31 -0400

    update files

--- AceCorpcustomIdentity/Controllers/HomeController.cs
+++ AceCorpcustomIdentity/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using AceCorpcustomIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace AceCorpcustomIdentity.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger) 
         {
+        
             _logger = logger;
+        
         }
 
         public IActionResult Index()
--- AceCorpcustomIdentity/ViewModels/RegisterVM.cs
+++ AceCorpcustomIdentity/ViewModels/RegisterVM.cs
@@ -9,7 +9,10 @@ namespace AceCorpCustomIdentity.ViewModels
         public required string Email { get; set; }
         public required string Password { get; set; }
         [Compare("Password", ErrorMessage ="Passwords don't match.")]
+        [Display(Name="Confirm Password")]
+        [DataType(DataType.Password)]
         public required string ConfirmPassword { get; set; }
+        [DataType(DataType.MultilineText)]
         public required string Address { get; set; }
     }
 }
